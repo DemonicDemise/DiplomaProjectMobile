7da0ee8cd05610c104520794543ec57bd8fb97d9
Mukash <pulatmukhammedaly@gmail.com>
2023-04-09 19:48:25 +0600

    first commit

---
+++ app/src/main/java/com/example/diploma/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.diploma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button signIn;
+    EditText email, password;
+    TextView signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        signIn = findViewById(R.id.login_btn);
+        email = findViewById(R.id.login_email);
+        password = findViewById(R.id.login_password);
+        signUp = findViewById(R.id.login_register);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+            }
+        });
+    }
+}
