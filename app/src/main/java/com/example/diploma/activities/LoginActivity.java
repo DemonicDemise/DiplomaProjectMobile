e39bfdf35491f92d2409751d8d8d86d5699534b6
Mukash <pulatmukhammedaly@gmail.com>
2023-04-17 17:12:21 +0600

    1. Navigation Inflater through binding

--- app/src/main/java/com/example/diploma/activities/LoginActivity.java
+++ app/src/main/java/com/example/diploma/activities/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.diploma;
+package com.example.diploma.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +13,8 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.diploma.MainActivity;
+import com.example.diploma.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
--- app/src/main/java/com/example/diploma/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/diploma/activities/RegistrationActivity.java
@@ -1,11 +1,10 @@
-package com.example.diploma;
+package com.example.diploma.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,13 +12,10 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.diploma.R;
 import com.example.diploma.models.UserModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class RegistrationActivity extends AppCompatActivity {
 
